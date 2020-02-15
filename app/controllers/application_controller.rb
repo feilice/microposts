fabe419e904fa5435afadfceec7dde44aea4d42b
satoshi.funayama <lala.un.azalea@gmail.com>
2020-02-15 01:54:33 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  
+  include SessionsHelper
+  
+  private
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to log_url
+    end
+  end
 end
