99e717def48e30b942344924b2c54a38cd0b90db
satoshi.funayama <lala.un.azalea@gmail.com>
2020-02-15 07:26:20 +0000

    signup repair

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,22 +10,23 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
-      flash[:success] = 'ユーザーを登録しました。'
+      flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
     else
-      flash[:danger] = "ユーザーの登録に失敗しました。"
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
   end
-  
-  private 
-  
+
+  private
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
